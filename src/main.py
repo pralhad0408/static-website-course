69d86bbb3bf1808cc9f686b0b0c9afd383436edb
pralhad0408 <fearlessandhigh@gmail.com>
2026-07-06 19:36:46 +0530

    Add static assets and ignored generated public directory

--- src/main.py
+++ src/main.py
@@ -1,10 +1,37 @@
+import os
+import shutil
 from textnode import TextNode, TextType
 
+def copy_static_to_public(src, dst):
+    if os.path.exists(dst):
+        shutil.rmtree(dst)
+
+    os.mkdir(dst)
+
+    copy_directory(src, dst)
+
+def copy_directory(src, dst):
+    for item in os.listdir(src):
+        src_path = os.path.join(src, item)
+        dst_path = os.path.join(dst, item)
+
+        if os.path.isfile(src_path):
+            print(f"Copying files: {src_path} -> {dst_path}")
+            shutil.copy(src_path, dst_path)
+        else:
+            print(f"Creating directory: {dst_path}")
+            os.mkdir(dst_path)
+            copy_directory(src_path, dst_path)
+
 def main():
+    copy_static_to_public("static", "public")
+
     node = TextNode(
         "This is some anchor text",
         TextType.LINK,
         "http://www.boot.dev"
     )
     print(node)
-main()
+
+if __name__ == "__main__":
+    main()
