06bd19df04c2092da60b24eccf654632e7bcd114
pralhad0408 <fearlessandhigh@gmail.com>
2026-07-06 19:49:13 +0530

    Implement recursive static asset copying

--- src/main.py
+++ src/main.py
@@ -1,8 +1,7 @@
 import os
 import shutil
-from textnode import TextNode, TextType
 
-def copy_static_to_public(src, dst):
+def copy_static_to_public(src: str, dst: str) -> None:
     if os.path.exists(dst):
         shutil.rmtree(dst)
 
@@ -10,13 +9,13 @@ def copy_static_to_public(src, dst):
 
     copy_directory(src, dst)
 
-def copy_directory(src, dst):
+def copy_directory(src: str, dst: str) -> None:
     for item in os.listdir(src):
         src_path = os.path.join(src, item)
         dst_path = os.path.join(dst, item)
 
         if os.path.isfile(src_path):
-            print(f"Copying files: {src_path} -> {dst_path}")
+            print(f"Copying file: {src_path} -> {dst_path}")
             shutil.copy(src_path, dst_path)
         else:
             print(f"Creating directory: {dst_path}")
@@ -25,13 +24,6 @@ def copy_directory(src, dst):
 
 def main():
     copy_static_to_public("static", "public")
-
-    node = TextNode(
-        "This is some anchor text",
-        TextType.LINK,
-        "http://www.boot.dev"
-    )
-    print(node)
 
 if __name__ == "__main__":
     main()
