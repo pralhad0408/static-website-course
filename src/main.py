1a2a8fc86bca3c1e5eb955fa6571775d9e4f326f
pralhad0408 <fearlessandhigh@gmail.com>
2026-07-13 17:55:48 +0530

    Generate HTML pages from markdown templates

--- src/main.py
+++ src/main.py
@@ -1,6 +1,34 @@
 import os
 import shutil
 
+from extract_title import extract_title
+from markdown_to_html_node import markdown_to_html_node
+
+def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+
+    with open(from_path, "r") as f:
+        markdown = f.read()
+
+    with open(template_path, "r") as f:
+        template = f.read()
+
+    html_node = markdown_to_html_node(markdown)
+    html = html_node.to_html()
+
+    title = extract_title(markdown)
+
+    full_html = template.replace("{{ Title }}", title)
+    full_html = full_html.replace("{{ Content }}", html)
+
+    dest_dir = os.path.dirname(dest_path)
+
+    if dest_dir:
+        os.makedirs(dest_dir, exist_ok=True)
+
+    with open(dest_path, "w") as f:
+        f.write(full_html)
+
 def copy_static_to_public(src: str, dst: str) -> None:
     if os.path.exists(dst):
         shutil.rmtree(dst)
@@ -24,6 +52,7 @@ def copy_directory(src: str, dst: str) -> None:
 
 def main():
     copy_static_to_public("static", "public")
+    generate_page("content/index.md", "template.html", "public/index.html")
 
 if __name__ == "__main__":
     main()
